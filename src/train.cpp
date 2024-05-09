99c8e6b403c81935236f784593065f5b6f3b27a0
VarvaraBelova23 <160231633+VarvaraBelova23@users.noreply.github.com>
2024-05-09 17:10:30 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,35 +1,60 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
-Train::Train() : countOp(0), first(nullptr) {}
+
+Train::Train() {
+  countOp = 0;
+  first = nullptr;
+}
+
+Train::~Train() {
+  Cage* current = first;
+  while (current->next != first) {
+    Cage* temp = current;
+    current = current->next;
+    delete temp;
+  }
+}
 
 void Train::addCage(bool light) {
-  Cage* newCage = new Cage{light, nullptr, nullptr};
-  if (!first) {
+  Cage* newCage = new Cage;
+  newCage->light = light;
+  newCage->next = nullptr;
+  if (first == nullptr) {
+    newCage->prev = nullptr;
     first = newCage;
     first->next = first;
-    first->prev = first;
   } else {
-    Cage* lastCage = first->prev;
-    lastCage->next = newCage;
-    newCage->prev = lastCage;
+    Cage* current = first;
+    while (current->next != first) {
+      current = current->next;
+    }
+    current->next = newCage;
+    newCage->prev = current;
     newCage->next = first;
     first->prev = newCage;
   }
 }
 
 int Train::getLength() {
-  if (!first) {
-    return 0;
-  }
-
-  int length = 1;
-  Cage* current = first->next;
-  while (current != first) {
-    length++;
+  Cage* current = first;
+  int length = 0;
+  current->light = true;
+  while (true) {
     current = current->next;
+    length++;
+    countOp++;
+    if (current->light == true) {
+      current->light = false;
+      int maxLength = length;
+      length = 0;
+      for (int i = 0; i < maxLength; i++) {
+        current = current->prev;
+        countOp++;
+      }
+      if (current->light == false)
+        return maxLength;
+    }
   }
-
-  return length;
 }
 
 int Train::getOpCount() {
